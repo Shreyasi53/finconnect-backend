13d95f0c0e8f440b73499938ac5dd005429ba743
Shreyasi53 <shreyasidutta53@gmail.com>
2026-05-25 20:15:18 +0530

    add mark as read functionality for notifications

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,14 +1,47 @@
 import { Notification } from "../models/notification.model.js";
+
+// GET NOTIFICATIONS
 const getNotifications = async (req, res) => {
    try {
-      const notifications = await Notification.find({
-         receiver: req.user._id
-      })
+      const notifications = await Notification.find({receiver: req.user._id})
       .populate("sender", "fullname username profileImage")
       .sort({ createdAt: -1 });
       return res.status(200).json({
          notifications
       });
+
+   } catch (error) {
+      return res.status(500).json({
+         message: "Something went wrong",
+         error: error.message
+      });
+   }
+};
+
+// MARK NOTIFICATION AS READ
+const markAsRead = async (req, res) => {
+   try {
+      const notification = await Notification.findById(
+         req.params.id
+      );
+      if (!notification) {
+         return res.status(404).json({
+            message: "Notification not found"
+         });
+      }
+      // ownership check
+      if (notification.receiver.toString() !== req.user._id.toString()){
+         return res.status(403).json({
+            message: "Unauthorized action"
+         });
+      }
+      notification.isRead = true;
+      await notification.save();
+      return res.status(200).json({
+         message: "Notification marked as read",
+         notification
+      });
+
    } catch (error) {
       return res.status(500).json({
          message: "Something went wrong",
@@ -18,5 +51,6 @@ const getNotifications = async (req, res) => {
 };
 
 export {
-   getNotifications
+   getNotifications,
+   markAsRead
 };
--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
-import { getNotifications } from "../controllers/notification.controller.js";
+import { getNotifications, markAsRead} from "../controllers/notification.controller.js";
 
 const router = Router();
 
 router.get("/", verifyJWT, getNotifications);
-
+router.route("/read/:id").patch(verifyJWT, markAsRead);
 export default router;
